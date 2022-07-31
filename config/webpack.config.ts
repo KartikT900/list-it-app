6cb9e0844888995482635f2de969bbb016b3f72e
Kartik <43693807+KartikT900@users.noreply.github.com>
2022-07-31 10:13:05 +0530

    feat: added webpack config (sans css loader)

---
+++ config/webpack.config.ts
@@ -0,0 +1,54 @@
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+import path from 'path';
+
+const devMode: boolean = process.env.NODE_ENV !== 'production';
+
+const config: webpack.Configuration =  {
+  devServer: {
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+    port: 8000,
+    client: {
+      overlay: true,
+      logging: "warn"
+    },
+    open: 'list/'
+    },
+    devtool: devMode
+      ? 'eval-cheap-module-source-map'
+      : 'cheap-module-source-map',
+  entry: ['./src/public-path.ts','./src/index.ts'],
+  mode: devMode ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template: `${path.join(__dirname, '../src')}/index.html`
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+    modules: ['node_modules', 'src']
+  },
+  output: {
+    path: path.join(__dirname, '../build'),
+    publicPath: '/',
+    filename: '[name].js',
+    chunkFilename: '[name].js'
+  },
+};
+
+export default config;
