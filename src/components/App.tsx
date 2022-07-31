4324dbc02f7aab5e9379e5a796447ed78bc9a5f3
Kartik <43693807+KartikT900@users.noreply.github.com>
2022-07-31 10:14:01 +0530

    feat: added basic app setup with App.tsx and index html

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App():React.ReactNode {
+    return <div>list it app</div>
+}
+
+export default App;
---
+++ src/public-path.ts
@@ -0,0 +1,4 @@
+declare var  __webpack_public_path:string; 
+
+// eslint-disable-next-line
+__webpack_public_path = '/';
