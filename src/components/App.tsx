4fc74fb3b91e3eddcff08b5afab26f67bc6a7aa9
Kartik <43693807+KartikT900@users.noreply.github.com>
2022-10-15 10:21:14 +0530

    fix: remove console logs for env variables

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 function App(): JSX.Element {
   const { loginWithRedirect } = useAuth0();
 
-  // eslint-disable-next-line no-console
-  console.log(process.env);
-  // eslint-disable-next-line no-console
-  console.log(process.env.AUTH0_CLIENTID, process.env.AUTH0_DOMAIN);
-
   return (
     <div>
       <button
